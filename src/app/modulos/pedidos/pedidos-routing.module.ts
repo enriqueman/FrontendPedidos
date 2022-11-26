43b9744b59a27715160b04b07f15f60796d85eb9
enriqueman <manzano.velasco217@gmail.com>
2022-11-26 13:36:22 -0500

    Rutas asignar pedido

--- src/app/modulos/pedidos/pedidos-routing.module.ts
+++ src/app/modulos/pedidos/pedidos-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsignarPedidoComponent } from './asignar-pedido/asignar-pedido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"asignar-pedido",
+    component:AsignarPedidoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
